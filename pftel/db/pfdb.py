773ca008c2e9939542345bc665d0151139860924
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2023-02-14 18:20:45 -0500

    WIP init and add list/init methods

--- pftel/db/pfdb.py
+++ pftel/db/pfdb.py
@@ -1,67 +1,5 @@
 from    pydantic            import BaseModel, Field
-from    models              import pacsSetupModel
-
-str_designNotes = """
-        This module simulates some external data base service, used
-        to simulate keeping some state information in the system.
-
-        Typical state includes information about PACS servers to
-        interact with, as well as information pertaining to setting
-        up the xinetd service etc (although the xinet service is
-        best setup using docker, still...)
-
-        The PFDB module returns model representations of data objects
-        relevant to the functioning of the pfdcm service.
-
-        In theory, no component of the `pfdcm` system will call on
-        state variables or structures directly, but will only interact
-        with this API.
-
-        Internally, the "db" is a pfstate object. There can be
-        typically only one such object per namespace, so the db
-        "wrapper" returns from this object in a manner that
-        does not require the caller to understand this internal
-        organization.
-
-"""
-str_apologies = """
-
-        Yes, dear reader, this is a module for keeping some type
-        of "state" information in the system.
-
-        Now, before you might go apoplectic at the idea that `pfdcm`
-        is now keeping state, before you might fall backwards in
-        righteous indignation, before you open you mouth (or twitch
-        your fingers) with thoughts of, "config data should be constant
-        over the lifetime of the process and should be set in the
-        environment and parsed at startup!"... to you I say, gently,
-
-        "Hold your horses."
-
-        And, "I know. You are not wrong."
-
-        I am, however, purposefully coding some concept of state which
-        ideally should probably be contained in a separate module (like
-        a MonogDB) for my own illustrative purposes.
-
-        The `pfdcm` system is not meant to scale. It is not meant to
-        ramp up over a multitude of environments. Strictly speaking
-        it can be designed so that part of its operational behaviour
-        is not coded as persistent state information (i.e. it can in
-        fact be totally stateless). It can be fully specified at startup
-        runtime with complete information to define its scope of
-        behaviour.
-
-        These are all true. However, I am choosing with eyes wide open
-        to provide some rudimentary exemplar of one way in which state
-        could in theory be kept in the system.
-
-        The idea with this module is to _simulate_ an external DB and
-        design the system around that. All calls to any state information
-        are thus routed through this module and should, in theory, be
-        easily updated in future to an actual separate DB.
-
-"""
+# from    models              import pacsSetupModel
 
 import  json
 from    datetime    import datetime
@@ -69,9 +7,11 @@ from    datetime    import datetime
 import  pfstate
 from    pfstate     import  S
 
+from    pfmisc      import C_snode
+
 import  pudb
 
-class DBdata(S):
+class DBstate(S):
     """
 
     All state data, i.e. trackers of information:
@@ -81,6 +21,30 @@ class DBdata(S):
     are kept in this object
 
     """
+
+    str_designNotes = """
+            This module simulates some external data base service, used
+            to save logging information to actual resources in the
+            storage medium.
+
+            Internally, the "db" is a pfstate object. There can be
+            typically only one such object per namespace, so the db
+            "wrapper" returns from this object in a manner that
+            does not require the caller to understand this internal
+            organization. This usage of pfstate is historical, and
+            should probably be discouraged for future use.
+
+    """
+    str_apologies = """
+
+            The idea with this module is to _simulate_ an external DB and
+            design the system around that. All calls to any state information
+            are thus routed through this module and should, in theory, be
+            easily updated in future to an actual separate DB.
+
+    """
+    DB : str = '/DB/service'
+
     def __init__(self, *args, **kwargs):
         """
         An object to hold some generic/global-ish system state, in C_snode
@@ -100,7 +64,7 @@ class DBdata(S):
         {
             'DB' :
             {
-                'telemetryService':
+                'service':
                 {
                     'default':
                     {
@@ -112,13 +76,12 @@ class DBdata(S):
         },
         *args, **kwargs)
 
-
 class PFdb():
     """
     An object that is meant to simulate interaction with some external
     (idiosyncratic) data base.
 
-    Actual data is kept in the DBdata object, and this class
+    Actual data is kept in the DBstate object, and this class
     provides a python API to manipulate data objects and
     and returns models of the objects where appropriate.
 
@@ -131,7 +94,6 @@ class PFdb():
     # and not in a per-object instance basis. There is afterall
     # only one DB per pfdcm session
     dobj_DB             = None
-
 
     # Data pertaining to the telemetry logger
     d_telemetryCore         : dict  = {
@@ -150,22 +112,27 @@ class PFdb():
         """
         self.str_loginName  = ''
         self.str_passwd     = ''
-
+        pudb.set_trace()
         for k,v in kwargs.items():
             if k == 'login' :   self.str_loginName  = v
             if k == 'passwd':   self.str_passwd     = v
 
         # Create the basic DB
-        PFdb.dobj_DB    = DBdata(*args, **dict(kwargs, useGlobalState = True))
+        PFdb.dobj_DB    = DBstate(*args, **dict(kwargs, useGlobalState = True))
+        self.DB         = PFdb.dobj_DB.T
 
         # Now add the default telemetry details
-        PFdb.dobj_DB.T.touch(
-            '/home/dicom/telmetryService/default/info',
-            PFdb.d_telemetryCore
+        self.DB.touch(
+            '%s/default/info' % DBstate.DB, PFdb.d_telemetryCore
         )
+        T_onDisk        = C_snode.C_stree.tree_load(pathDiskRoot = self.str_FSprefix)
+        d_T:dict        = {}
+        d_T.update(next(T_onDisk.__iter__(node = '/')))
+        self.DB.tree_save(startPath = '/', pathDiskRoot = self.str_FSprefix)
+        PFdb.dobj_DB.T.initFromDict(d_T)
 
     def telemetryService_listObjs(self)-> list:
-        return list(PFdb.dobj_DB.T.lstr_lsnode('/DB/telemetryService'))
+        return list(self.DB.lstr_lsnode(DBstate.DB))
 
     def telemetryService_initObj(self,
             str_objName : str,
@@ -176,33 +143,51 @@ class PFdb():
         """
         str_message     : str   = ""
         if str_objName not in self.telemetryService_listObjs():
-            PFdb.dobj_DB.T.mkdir('/DB/telemetryService/%s'   % str_objName)
+            PFdb.dobj_DB.T.mkdir('%s/%s'   % (DBstate.DB, str_objName))
             PFdb.dobj_DB.T.touch(
-                    '/DB/telemetryService/%s/json_created'   % str_objName,
+                    '%s/%s/json_created'   % (DBstate.DB, str_objName),
                     json.dumps({'time' : '%s' % datetime.now()})
             )
             str_message     = "New object '%s' created" % str_objName
         else:
             str_message     = "Existing object '%s' modified" % str_objName
 
-        PFdb.dobj_DB.T.touch('/DB/telemetryService/%s/info' % str_objName, d_data)
+        PFdb.dobj_DB.T.touch('%s/%s/info' % (DBstate.DB, str_objName), d_data)
         PFdb.dobj_DB.T.touch(
-                '/DB/telemetryService/%s/json_modified'      % str_objName,
+                '%s/%s/json_modified'     % (DBstate.DB, str_objName),
                 json.dumps({'time' : '%s' % datetime.now()})
         )
         return {
-            'info'          : PFdb.dobj_DB.T.cat(
-                                '/DB/telemetryService/%s/info'           % str_objName
+            'info'          : self.DB.cat(
+                                '%s/%s/info'           % (DBstate.DB, str_objName)
                             ),
-            'time_created'  : json.loads(PFdb.dobj_DB.T.cat(
-                                '/DB/telemetryService/%s/json_created'   % str_objName)
+            'time_created'  : json.loads(self.DB.cat(
+                                '%s/%s/json_created'   % (DBstate.DB, str_objName))
                             ),
-            'time_modified' : json.loads(PFdb.dobj_DB.T.cat(
-                                '/DB/telemetryService/%s/json_modified'  % str_objName)
+            'time_modified' : json.loads(self.DB.cat(
+                                '%s/%s/json_modified'  % (DBstate.DB, str_objName))
                             ),
             'message'       : str_message
         }
 
+    def telemetryService_info(self, str_objName) -> dict:
+        """
+        Return a model conforming representation of a given
+        log element object
+        """
+        if str_objName in self.telemetryService_listObjs():
+            return {
+                'info'          : self.DB.cat(
+                                    '%s/%s/info'           % (DBstate.DB, str_objName)
+                                ),
+                'time_created'  : json.loads(self.DB.cat(
+                                    '%s/%s/json_created'   % (DBstate.DB, str_objName))
+                                ),
+                'time_modified' : json.loads(self.DB.cat(
+                                    '%s/%s/json_modified'  % (DBstate.DB, str_objName))
+                                ),
+                'message'       : "Service information for '%s'"        % str_objName
+            }
 
     def PACSservice_listObjs(self) -> list:
         """
