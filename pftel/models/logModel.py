0a0a6a1785646be6c887111963d55ad81542bf02
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2023-02-14 18:19:59 -0500

    Remove superfluous classes

--- pftel/models/logModel.py
+++ pftel/models/logModel.py
@@ -12,6 +12,15 @@ from    pathlib             import Path
 from    db                  import pfdb
 import  pudb
 
+
+class logCore(BaseModel):
+    """Model for the core log info saved to DB"""
+    url                                 : str = ""
+    username                            : str = ""
+    password                            : str = ""
+    dbDir                               : str = ""
+    telemetryDir                        : str = ""
+
 class logFile(BaseModel):
     """Model for the log file"""
     path                                : Path
@@ -24,9 +33,10 @@ class logSimple(BaseModel):
 
 class logStructured(BaseModel):
     """A simple structured log model"""
-    id                                  : str   = ""
-    appname                             : str   = ""
-    exectime                            : float = 0.0
+    logObject                           : str   = "default"
+    logName                             : str   = ""
+    appName                             : str   = ""
+    execTime                            : float = 0.0
     extra                               : str   = ""
 
 class logResponse(BaseModel):
@@ -35,50 +45,18 @@ class logResponse(BaseModel):
     echo                                : str
     timestamp                           : str
 
-class PACSqueryCore(BaseModel):
-    """The PACS Query model"""
-    AccessionNumber                     : str   = ""
-    PatientID                           : str   = ""
-    PatientName                         : str   = ""
-    PatientBirthDate                    : str   = ""
-    PatientAge                          : str   = ""
-    PatientSex                          : str   = ""
-    StudyDate                           : str   = ""
-    StudyDescription                    : str   = ""
-    StudyInstanceUID                    : str   = ""
-    Modality                            : str   = ""
-    ModalitiesInStudy                   : str   = ""
-    PerformedStationAETitle             : str   = ""
-    NumberOfSeriesRelatedInstances      : str   = ""
-    InstanceNumber                      : str   = ""
-    SeriesDate                          : str   = ""
-    SeriesDescription                   : str   = ""
-    SeriesInstanceUID                   : str   = ""
-    ProtocolName                        : str   = ""
-    AcquisitionProtocolDescription      : str   = ""
-    AcquisitionProtocolName             : str   = ""
-    withFeedBack                        : bool  = False
-    then                                : str   = ""
-    thenArgs                            : str   = ""
-    dblogbasepath                       : str   = "/home/dicom/log"
-    json_response                       : bool  = True
-
-class PACSasync(BaseModel):
-    """A model returned when an async PACS directive is indicated"""
-    directiveType                       : str   = "async"
-    response                            : dict
-    timestamp                           : str
-    PACSdirective                       : dict
-
 class time(BaseModel):
     """A simple model that has a time string field"""
     time            : str
 
-class PACSqueyReturnModel(BaseModel):
+class logReturnModel(BaseModel):
     """
     A full model that is returned from a query call
     """
-    response        : str
+    info            : logCore
+    time_created    : time
+    time_modified   : time
+    message         : str
 
 # Some "helper" classes
 class ValueStr(BaseModel):
